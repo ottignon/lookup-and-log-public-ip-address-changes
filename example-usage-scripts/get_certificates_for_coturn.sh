3d0a09b62524f12fe932d54daf7829aae2527e73
ottignon <ottignon@outlook.com>
2021-04-29 14:07:38 +0100

    Update get_certificates_for_coturn.sh

--- example-usage-scripts/get_certificates_for_coturn.sh
+++ example-usage-scripts/get_certificates_for_coturn.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 certbot renew --webroot --preferred-challenges http --post-hook "/etc/haproxy/scripts/haproxy_ssl_consolidation.sh && rc-service haproxy reload" --quiet
-alpine-haproxy-1:~# cat /etc/haproxy/scripts/haproxy_ssl_consolidation.sh
-#!/bin/bash
-# Loop through all Let's Encrypt certificates
-for CERTIFICATE in `find /etc/letsencrypt/live/* -type d`; do
-  CERTIFICATE=`basename $CERTIFICATE`
-  # Combine certificate and private key to single file
-  cat /etc/letsencrypt/live/$CERTIFICATE/fullchain.pem /etc/letsencrypt/live/$CERTIFICATE/privkey.pem > /etc/haproxy/ssl/$CERTIFICATE.pem
-done
