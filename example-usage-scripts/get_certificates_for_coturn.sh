bddba2f861bc9a4dc0540273ac3a3f10eb76bf0b
ottignon <ottignon@outlook.com>
2021-04-29 14:16:49 +0100

    Update get_certificates_for_coturn.sh

--- example-usage-scripts/get_certificates_for_coturn.sh
+++ example-usage-scripts/get_certificates_for_coturn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-certbot renew --webroot --preferred-challenges http --post-hook "/etc/haproxy/scripts/haproxy_ssl_consolidation.sh && rc-service haproxy reload" --quiet
+# Example where HAProxy is being used for on the server
+certbot renew --webroot --preferred-challenges http --post-hook "/usr/local/bin/lookup-and-log-public-ip-address-changes/example-usage-scripts/haproxy_ssl_consolidation.sh && rc-service haproxy reload" --quiet
