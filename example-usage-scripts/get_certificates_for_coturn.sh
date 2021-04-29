014e28bec5d3b12928266b59732c660d4605ae69
ottignon <ottignon@outlook.com>
2021-04-29 14:25:55 +0100

    Update get_certificates_for_coturn.sh

--- example-usage-scripts/get_certificates_for_coturn.sh
+++ example-usage-scripts/get_certificates_for_coturn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Example where HAProxy is being used for on the server
+# Example where HAProxy is being used on the server, certbot needs a backend where it can store challenge file such as .well-known/acme-challenge on NGINX or Apache
+# Alternatively certbot can be used in standalone mode
 certbot renew --webroot --preferred-challenges http --post-hook "/usr/local/bin/lookup-and-log-public-ip-address-changes/example-usage-scripts/haproxy_ssl_consolidation.sh && rc-service haproxy reload" --quiet
