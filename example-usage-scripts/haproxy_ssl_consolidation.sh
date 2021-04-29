0e7a14f4c435acf2d85ccd6ce403fc7dc9ae261d
ottignon <ottignon@outlook.com>
2021-04-29 14:07:06 +0100

    Create haproxy_ssl_consolidation.sh

---
+++ example-usage-scripts/haproxy_ssl_consolidation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Loop through all Let's Encrypt certificates
+for CERTIFICATE in `find /etc/letsencrypt/live/* -type d`; do
+  CERTIFICATE=`basename $CERTIFICATE`
+  # Combine certificate and private key to single file
+  cat /etc/letsencrypt/live/$CERTIFICATE/fullchain.pem /etc/letsencrypt/live/$CERTIFICATE/privkey.pem > /etc/haproxy/ssl/$CERTIFICATE.pem
+done
